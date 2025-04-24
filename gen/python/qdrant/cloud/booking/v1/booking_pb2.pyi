3bfea98e42398d00b52638157e4662a3f91ad665
Toni Reina <toni.reina@qdrant.com>
2025-04-24 17:40:56 +0200

    Update `qdrant.cloud.booking.v1` to use enums (#64)

--- gen/python/qdrant/cloud/booking/v1/booking_pb2.pyi
+++ gen/python/qdrant/cloud/booking/v1/booking_pb2.pyi
@@ -2,11 +2,21 @@ from buf.validate import validate_pb2 as _validate_pb2
 from google.api import annotations_pb2 as _annotations_pb2
 from qdrant.cloud.common.v1 import common_pb2 as _common_pb2
 from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class PackageStatus(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    PACKAGE_STATUS_UNSPECIFIED: _ClassVar[PackageStatus]
+    PACKAGE_STATUS_ACTIVE: _ClassVar[PackageStatus]
+    PACKAGE_STATUS_DEACTIVATED: _ClassVar[PackageStatus]
+PACKAGE_STATUS_UNSPECIFIED: PackageStatus
+PACKAGE_STATUS_ACTIVE: PackageStatus
+PACKAGE_STATUS_DEACTIVATED: PackageStatus
 
 class ListPackagesRequest(_message.Message):
     __slots__ = ("account_id", "cloud_provider", "cloud_region", "statuses")
@@ -17,8 +27,8 @@ class ListPackagesRequest(_message.Message):
     account_id: str
     cloud_provider: str
     cloud_region: str
-    statuses: _containers.RepeatedScalarFieldContainer[str]
-    def __init__(self, account_id: _Optional[str] = ..., cloud_provider: _Optional[str] = ..., cloud_region: _Optional[str] = ..., statuses: _Optional[_Iterable[str]] = ...) -> None: ...
+    statuses: _containers.RepeatedScalarFieldContainer[PackageStatus]
+    def __init__(self, account_id: _Optional[str] = ..., cloud_provider: _Optional[str] = ..., cloud_region: _Optional[str] = ..., statuses: _Optional[_Iterable[_Union[PackageStatus, str]]] = ...) -> None: ...
 
 class ListPackagesResponse(_message.Message):
     __slots__ = ("items",)
@@ -55,8 +65,8 @@ class Package(_message.Message):
     resource_configuration: ResourceConfiguration
     currency: str
     unit_int_price_per_hour: int
-    status: str
-    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[str] = ..., resource_configuration: _Optional[_Union[ResourceConfiguration, _Mapping]] = ..., currency: _Optional[str] = ..., unit_int_price_per_hour: _Optional[int] = ..., status: _Optional[str] = ...) -> None: ...
+    status: PackageStatus
+    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ..., type: _Optional[str] = ..., resource_configuration: _Optional[_Union[ResourceConfiguration, _Mapping]] = ..., currency: _Optional[str] = ..., unit_int_price_per_hour: _Optional[int] = ..., status: _Optional[_Union[PackageStatus, str]] = ...) -> None: ...
 
 class ResourceConfiguration(_message.Message):
     __slots__ = ("ram", "cpu", "disk")
