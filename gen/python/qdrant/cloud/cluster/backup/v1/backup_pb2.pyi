04f6d56f633d132d5cf49f2d5eb52998b1dc5ed5
Robert-Stam <robert.stam@qdrant.com>
2025-08-27 11:29:32 +0200

    Added events option to the API
    And slightly adjust the options to embed metadata and support for actions

--- gen/python/qdrant/cloud/cluster/backup/v1/backup_pb2.pyi
+++ gen/python/qdrant/cloud/cluster/backup/v1/backup_pb2.pyi
@@ -6,6 +6,7 @@ from google.protobuf import duration_pb2 as _duration_pb2
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from qdrant.cloud.cluster.v1 import cluster_pb2 as _cluster_pb2
 from qdrant.cloud.common.v1 import common_pb2 as _common_pb2
+from qdrant.cloud.event.v1 import events_pb2 as _events_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
--- gen/python/qdrant/cloud/event/v1/events_pb2.pyi
+++ gen/python/qdrant/cloud/event/v1/events_pb2.pyi
@@ -47,7 +47,7 @@ class EventOptions(_message.Message):
     resource_id_field: str
     resource_url_template: str
     additional_context_fields: _containers.ScalarMap[str, str]
-    def __init__(self, event_type: _Optional[_Union[EventType, str]] = ..., resource_type: _Optional[str] = ..., status_only: bool = ..., resource_id_field: _Optional[str] = ..., resource_url_template: _Optional[str] = ..., additional_context_fields: _Optional[_Mapping[str, str]] = ...) -> None: ...
+    def __init__(self, event_type: _Optional[_Union[EventType, str]] = ..., resource_type: _Optional[str] = ..., status_only: _Optional[bool] = ..., resource_id_field: _Optional[str] = ..., resource_url_template: _Optional[str] = ..., additional_context_fields: _Optional[_Mapping[str, str]] = ...) -> None: ...
 
 class Event(_message.Message):
     __slots__ = ("id", "created_at", "actor_id", "actor_type", "account_id", "event_type", "resource_type", "status_only", "resource_id", "resource_url", "additional_context")
@@ -80,4 +80,4 @@ class Event(_message.Message):
     resource_id: str
     resource_url: str
     additional_context: _containers.ScalarMap[str, str]
-    def __init__(self, id: _Optional[str] = ..., created_at: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., actor_id: _Optional[str] = ..., actor_type: _Optional[_Union[_common_pb2.ActorType, str]] = ..., account_id: _Optional[str] = ..., event_type: _Optional[_Union[EventType, str]] = ..., resource_type: _Optional[str] = ..., status_only: bool = ..., resource_id: _Optional[str] = ..., resource_url: _Optional[str] = ..., additional_context: _Optional[_Mapping[str, str]] = ...) -> None: ...
+    def __init__(self, id: _Optional[str] = ..., created_at: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., actor_id: _Optional[str] = ..., actor_type: _Optional[_Union[_common_pb2.ActorType, str]] = ..., account_id: _Optional[str] = ..., event_type: _Optional[_Union[EventType, str]] = ..., resource_type: _Optional[str] = ..., status_only: _Optional[bool] = ..., resource_id: _Optional[str] = ..., resource_url: _Optional[str] = ..., additional_context: _Optional[_Mapping[str, str]] = ...) -> None: ...
