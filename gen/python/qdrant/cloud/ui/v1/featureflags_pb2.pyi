0ef3e6914836c7cb6385719dc965599c3ba1d55f
Daniel Fernandez Arias <daniel.fernandez@qdrant.com>
2026-03-16 11:49:08 -0600

    Remove cluster creation page FF from GetFeatureFlagsResponse

--- gen/python/qdrant/cloud/ui/v1/featureflags_pb2.pyi
+++ gen/python/qdrant/cloud/ui/v1/featureflags_pb2.pyi
@@ -13,15 +13,13 @@ class GetFeatureFlagsRequest(_message.Message):
 class GetFeatureFlagsResponse(_message.Message):
     __slots__ = ()
     CLUSTER_CREATION_ENABLED_FIELD_NUMBER: _ClassVar[int]
-    CLUSTER_CREATION_PAGE_ENABLED_FIELD_NUMBER: _ClassVar[int]
     BILLING_CYCLES_ENABLED_FIELD_NUMBER: _ClassVar[int]
     ENTITLEMENT_CANCEL_FLOW_ENABLED_FIELD_NUMBER: _ClassVar[int]
     QDRANT_CLUSTERS_QUERYING_ENABLED_FIELD_NUMBER: _ClassVar[int]
     CLUSTER_STORAGE_TIER_CONFIGURATION_ENABLED_FIELD_NUMBER: _ClassVar[int]
     cluster_creation_enabled: bool
-    cluster_creation_page_enabled: bool
     billing_cycles_enabled: bool
     entitlement_cancel_flow_enabled: bool
     qdrant_clusters_querying_enabled: bool
     cluster_storage_tier_configuration_enabled: bool
-    def __init__(self, cluster_creation_enabled: _Optional[bool] = ..., cluster_creation_page_enabled: _Optional[bool] = ..., billing_cycles_enabled: _Optional[bool] = ..., entitlement_cancel_flow_enabled: _Optional[bool] = ..., qdrant_clusters_querying_enabled: _Optional[bool] = ..., cluster_storage_tier_configuration_enabled: _Optional[bool] = ...) -> None: ...
+    def __init__(self, cluster_creation_enabled: _Optional[bool] = ..., billing_cycles_enabled: _Optional[bool] = ..., entitlement_cancel_flow_enabled: _Optional[bool] = ..., qdrant_clusters_querying_enabled: _Optional[bool] = ..., cluster_storage_tier_configuration_enabled: _Optional[bool] = ...) -> None: ...
