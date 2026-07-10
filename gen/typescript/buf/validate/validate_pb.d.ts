68e6f3895f6424d102da23fff4d0baa3ac9b7a27
Robert-Stam <robert.stam@qdrant.com>
2026-07-10 09:18:51 +0200

    make generate

--- gen/typescript/buf/validate/validate_pb.d.ts
+++ gen/typescript/buf/validate/validate_pb.d.ts
@@ -1644,7 +1644,7 @@ export declare type UInt64Rules = Message<"buf.validate.UInt64Rules"> & {
    * message MyUInt64 {
    *   uint64 value = 1 [
    *     (buf.validate.field).uint64.example = 1,
-   *     (buf.validate.field).uint64.example = -10
+   *     (buf.validate.field).uint64.example = 10
    *   ];
    * }
    * ```
@@ -2736,8 +2736,8 @@ export declare type BoolRules = Message<"buf.validate.BoolRules"> & {
    * ```proto
    * message MyBool {
    *   bool value = 1 [
-   *     (buf.validate.field).bool.example = 1,
-   *     (buf.validate.field).bool.example = 2
+   *     (buf.validate.field).bool.example = true,
+   *     (buf.validate.field).bool.example = false
    *   ];
    * }
    * ```
@@ -2967,7 +2967,7 @@ export declare type StringRules = Message<"buf.validate.StringRules"> & {
    * ```proto
    * message MyString {
    *   // must be in list ["apple", "banana"]
-   *   string value = 1 [(buf.validate.field).string.in = "apple", (buf.validate.field).string.in = "banana"];
+   *   string value = 1 [(buf.validate.field).string = { in: ["apple", "banana"] }];
    * }
    * ```
    *
@@ -2982,7 +2982,7 @@ export declare type StringRules = Message<"buf.validate.StringRules"> & {
    * ```proto
    * message MyString {
    *   // value must not be in list ["orange", "grape"]
-   *   string value = 1 [(buf.validate.field).string.not_in = "orange", (buf.validate.field).string.not_in = "grape"];
+   *   string value = 1 [(buf.validate.field).string = { not_in: ["orange", "grape"] }];
    * }
    * ```
    *
@@ -3638,7 +3638,7 @@ export declare type BytesRules = Message<"buf.validate.BytesRules"> & {
    * ```proto
    * message MyBytes {
    *   // value must in ["\x01\x02", "\x02\x03", "\x03\x04"]
-   *   optional bytes value = 1 [(buf.validate.field).bytes.in = {"\x01\x02", "\x02\x03", "\x03\x04"}];
+   *   optional bytes value = 1 [(buf.validate.field).bytes = { in: ["\x01\x02", "\x02\x03", "\x03\x04"] }];
    * }
    * ```
    *
@@ -3655,7 +3655,7 @@ export declare type BytesRules = Message<"buf.validate.BytesRules"> & {
    * ```proto
    * message MyBytes {
    *   // value must not in ["\x01\x02", "\x02\x03", "\x03\x04"]
-   *   optional bytes value = 1 [(buf.validate.field).bytes.not_in = {"\x01\x02", "\x02\x03", "\x03\x04"}];
+   *   optional bytes value = 1 [(buf.validate.field).bytes = { not_in: ["\x01\x02", "\x02\x03", "\x03\x04"] }];
    * }
    * ```
    *
@@ -3868,8 +3868,10 @@ export declare type EnumRules = Message<"buf.validate.EnumRules"> & {
    * }
    *
    * message MyMessage {
+   *   MyEnum value = 1 [
    *     (buf.validate.field).enum.example = 1,
    *     (buf.validate.field).enum.example = 2
+   *   ];
    * }
    * ```
    *
@@ -4126,7 +4128,7 @@ export declare type DurationRules = Message<"buf.validate.DurationRules"> & {
    * ```proto
    * message MyDuration {
    *   // value must equal 5s
-   *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.const = "5s"];
+   *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.const = { seconds: 5 }];
    * }
    * ```
    *
@@ -4146,7 +4148,7 @@ export declare type DurationRules = Message<"buf.validate.DurationRules"> & {
      * ```proto
      * message MyDuration {
      *   // must be less than 5s
-     *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.lt = "5s"];
+     *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.lt = { seconds: 5 }];
      * }
      * ```
      *
@@ -4163,7 +4165,7 @@ export declare type DurationRules = Message<"buf.validate.DurationRules"> & {
      * ```proto
      * message MyDuration {
      *   // must be less than or equal to 10s
-     *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.lte = "10s"];
+     *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.lte = { seconds: 10 }];
      * }
      * ```
      *
@@ -4236,7 +4238,9 @@ export declare type DurationRules = Message<"buf.validate.DurationRules"> & {
    * ```proto
    * message MyDuration {
    *   // must be in list [1s, 2s, 3s]
-   *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.in = ["1s", "2s", "3s"]];
+   *   google.protobuf.Duration value = 1 [(buf.validate.field).duration = {
+   *     in: [{ seconds: 1 }, { seconds: 2 }, { seconds: 3 }]
+   *   }];
    * }
    * ```
    *
@@ -4253,7 +4257,9 @@ export declare type DurationRules = Message<"buf.validate.DurationRules"> & {
    * ```proto
    * message MyDuration {
    *   // value must not be in list [1s, 2s, 3s]
-   *   google.protobuf.Duration value = 1 [(buf.validate.field).duration.not_in = ["1s", "2s", "3s"]];
+   *   google.protobuf.Duration value = 1 [(buf.validate.field).duration = {
+   *     not_in: [{ seconds: 1 }, { seconds: 2 }, { seconds: 3 }]
+   *   }];
    * }
    * ```
    *
@@ -4270,7 +4276,7 @@ export declare type DurationRules = Message<"buf.validate.DurationRules"> & {
    * message MyDuration {
    *   google.protobuf.Duration value = 1 [
    *     (buf.validate.field).duration.example = { seconds: 1 },
-   *     (buf.validate.field).duration.example = { seconds: 2 },
+   *     (buf.validate.field).duration.example = { seconds: 2 }
    *   ];
    * }
    * ```
@@ -4361,7 +4367,7 @@ export declare type FieldMaskRules = Message<"buf.validate.FieldMaskRules"> & {
    * message MyFieldMask {
    *   google.protobuf.FieldMask value = 1 [
    *     (buf.validate.field).field_mask.example = { paths: ["a", "b"] },
-   *     (buf.validate.field).field_mask.example = { paths: ["c.a", "d"] },
+   *     (buf.validate.field).field_mask.example = { paths: ["c.a", "d"] }
    *   ];
    * }
    * ```
@@ -4542,7 +4548,7 @@ export declare type TimestampRules = Message<"buf.validate.TimestampRules"> & {
    * message MyTimestamp {
    *   google.protobuf.Timestamp value = 1 [
    *     (buf.validate.field).timestamp.example = { seconds: 1672444800 },
-   *     (buf.validate.field).timestamp.example = { seconds: 1672531200 },
+   *     (buf.validate.field).timestamp.example = { seconds: 1672531200 }
    *   ];
    * }
    * ```
@@ -4670,7 +4676,7 @@ export declare type Violation = Message<"buf.validate.Violation"> & {
    *   bool b = 2 [(buf.validate.field).cel = {
    *     id: "custom_rule",
    *     expression: "!this ? 'b must be true': ''"
-   *   }]
+   *   }];
    * }
    * ```
    *
@@ -5033,7 +5039,7 @@ export declare const field: GenExtension<FieldOptions, FieldRules>;
  *
  * ```proto
  * extend buf.validate.Int32Rules {
- *   bool is_zero [(buf.validate.predefined).cel = {
+ *   bool is_zero = 1001 [(buf.validate.predefined).cel = {
  *     id: "int32.is_zero",
  *     message: "must be zero",
  *     expression: "!rule || this == 0",
