ace8a2991eef8c47221eafc7d707a2933e2e2f62
Fabrizio Bettinelli <fabribettinelli@gmail.com>
2026-07-22 13:59:02 -0300

    CRC-2198 | Surface UNKNOWN status from cluster (#437)

--- gen/python/qdrant/cloud/cluster/v1/cluster_pb2.pyi
+++ gen/python/qdrant/cloud/cluster/v1/cluster_pb2.pyi
@@ -92,6 +92,7 @@ class ClusterPhase(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     CLUSTER_PHASE_FAILED_TO_SYNC: _ClassVar[ClusterPhase]
     CLUSTER_PHASE_NOT_FOUND: _ClassVar[ClusterPhase]
     CLUSTER_PHASE_DELETING: _ClassVar[ClusterPhase]
+    CLUSTER_PHASE_UNKNOWN: _ClassVar[ClusterPhase]
 
 class ClusterNodeState(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = ()
@@ -170,6 +171,7 @@ CLUSTER_PHASE_MANUAL_MAINTENANCE: ClusterPhase
 CLUSTER_PHASE_FAILED_TO_SYNC: ClusterPhase
 CLUSTER_PHASE_NOT_FOUND: ClusterPhase
 CLUSTER_PHASE_DELETING: ClusterPhase
+CLUSTER_PHASE_UNKNOWN: ClusterPhase
 CLUSTER_NODE_STATE_UNSPECIFIED: ClusterNodeState
 CLUSTER_NODE_STATE_STARTING: ClusterNodeState
 CLUSTER_NODE_STATE_HEALTHY: ClusterNodeState
